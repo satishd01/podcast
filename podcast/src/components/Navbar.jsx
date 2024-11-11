0941947dc63dfc24f339fca54f502901c9f4924d
7vTr0x7 <7vTrox7@gmail.com>
2024-11-11 16:15:19 +0530

    fix: slider

--- podcast/src/components/Navbar.jsx
+++ podcast/src/components/Navbar.jsx
@@ -3,25 +3,33 @@ import { CiSearch } from "react-icons/ci";
 import { IoIosNotifications } from "react-icons/io";
 import { HiMenu } from "react-icons/hi";
 import { RxCross2 } from "react-icons/rx";
+import { useDispatch } from "react-redux";
+import { toggleSlider } from "../app/slices/sliderSlice";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleIsUserViewOpen = () => {
+    dispatch(toggleSlider());
+  };
 
   return (
-    <nav className="grid grid-cols-12  text-white select-none">
+    <nav className="grid grid-cols-12 text-white select-none">
       <div className="md:col-span-2 col-span-3 md:bg-[#222222] bg-[#151515] flex px-8 py-3 text-lg items-center cursor-pointer">
-        LOGO
+        <p onClick={handleIsUserViewOpen}>LOGO</p>
       </div>
 
-      <div className="md:col-span-10 col-span-9 md:hidden bg-[#151515] flex justify-end items-center px-4 py-3 ">
+      {/* Hamburger for Mobile */}
+      <div className="md:col-span-10 col-span-9 md:hidden bg-[#151515] flex justify-end items-center px-4 py-3">
         {isMenuOpen ? (
           <RxCross2
-            className="text-3xl cursor-pointer"
+            className="text-3xl cursor-pointer transform transition-all duration-300 ease-in-out"
             onClick={() => setIsMenuOpen(false)}
           />
         ) : (
           <HiMenu
-            className="text-3xl cursor-pointer"
+            className="text-3xl cursor-pointer transform transition-all duration-300 ease-in-out"
             onClick={() => setIsMenuOpen(true)}
           />
         )}
@@ -30,7 +38,7 @@ const Navbar = () => {
       <div
         className={`md:col-span-10 col-span-12 bg-[#151515] ${
           isMenuOpen ? "block" : "hidden"
-        } md:grid md:grid-cols-12 flex-col justify-between items-center gap-16 px-14  py-2`}>
+        } md:grid md:grid-cols-12 flex-col justify-between items-center gap-16 px-14 py-2`}>
         <div className="flex md:col-span-8 md:flex-row flex-col w-full gap-4 justify-between items-center">
           {["All", "Podcasts", "Stories", "Audio Book"].map((item) => (
             <p
@@ -42,12 +50,12 @@ const Navbar = () => {
         </div>
 
         <div className="md:col-span-4 flex items-center gap-10 justify-between w-full">
-          <div className="bg-black py-2  w-full md:mt-0 mt-3 px-4 rounded-3xl flex justify-between items-center cursor-pointer">
+          <div className="bg-black py-2 w-full md:mt-0 mt-3 px-4 rounded-3xl flex justify-between items-center cursor-pointer">
             <p>Search</p>
             <CiSearch className="text-xl " />
           </div>
 
-          <div className="text-2xl  md:mt-0 mt-3 flex justify-end cursor-pointer">
+          <div className="text-2xl md:mt-0 mt-3 flex justify-end cursor-pointer">
             <IoIosNotifications />
           </div>
         </div>
--- podcast/src/pages/Home/Home.jsx
+++ podcast/src/pages/Home/Home.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
 
 const Home = () => {
-  const [isUserViewOpen, setIsUserViewOpen] = useState(false);
+  const isUserViewOpen = useSelector((state) => state.slider.isSliderOpen);
 
   return (
     <>
       <Navbar />
       <div className="grid grid-cols-12">
         <div
-          className={`md:col-span-2 text-gray-50 bg-[#222222] ${
+          className={`${
+            isUserViewOpen ? "md:col-span-2" : "md:hidden"
+          } absolute text-gray-50 bg-[#222222] ${
             isUserViewOpen
-              ? "z-40 absolute w-full md:mt-0 col-span-12 h-[100vh]"
-              : "hidden col-span-0"
+              ? "z-40  left-0 md:w-full w-6/12 md:mt-0 col-span-12 h-[100vh] transform transition-all duration-500 ease-in-out opacity-100"
+              : "  left-[-100%] opacity-0 md:relative col-span-0 transition-all duration-500 ease-in-out"
           } md:block md:relative md:z-0`}>
           <div className="p-4">
             <p>Sidebar Content</p>
           </div>
         </div>
 
-        <div className="md:col-span-10 col-span-12 bg-black relative  h-[100vh]">
-          {!isUserViewOpen && (
-            <button
-              onClick={() => setIsUserViewOpen(true)}
-              className="md:hidden p-2 bg-gray-700 rounded m-4">
-              Open Sidebar
-            </button>
-          )}
+        <div
+          className={`${
+            isUserViewOpen ? "md:col-span-10" : "md:col-span-12"
+          } col-span-12 text-white bg-black relative  h-[100vh]`}>
+          <p>data</p>
         </div>
       </div>
     </>
