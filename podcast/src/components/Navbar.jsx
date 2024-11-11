c12a916dc3aedc9c6abcce4bf5872f59140b648c
7vTr0x7 <7vTrox7@gmail.com>
2024-11-11 15:14:40 +0530

    feat: Navbar

--- podcast/src/components/Navbar.jsx
+++ podcast/src/components/Navbar.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { IoIosNotifications } from "react-icons/io";
+import { HiMenu } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="grid grid-cols-12 text-white">
-      <div className="col-span-2 bg-[#222222] flex px-8 py-3 text-lg items-center">
+    <nav className="grid grid-cols-12  text-white">
+      <div className="md:col-span-2 col-span-3 bg-[#222222] flex px-8 py-3 text-lg items-center">
         LOGO
       </div>
-      <div className="col-span-10 bg-[#151515] flex justify-between items-center gap-16 px-14 py-2">
-        <div className="flex w-full gap-4 justify-between items-center">
-          <p className="text-center border border-white py-1  w-full rounded-md text-sm bg-black">
-            All
-          </p>
-          <p className="text-center border border-white py-1  w-full rounded-md text-sm bg-black">
-            Podcasts
-          </p>
-          <p className="text-center border border-white py-1  w-full rounded-md text-sm bg-black">
-            Stories
-          </p>
-          <p className="text-center border border-white py-1  w-full rounded-md text-sm bg-black">
-            Audio Book
-          </p>
+
+      <div className="md:col-span-10 col-span-9 md:hidden bg-[#151515] flex justify-end items-center px-4 py-3">
+        {isMenuOpen ? (
+          <RxCross2 className="text-3xl" onClick={() => setIsMenuOpen(false)} />
+        ) : (
+          <HiMenu className="text-3xl" onClick={() => setIsMenuOpen(true)} />
+        )}
+      </div>
+
+      <div
+        className={`md:col-span-10 col-span-12 bg-[#151515] ${
+          isMenuOpen ? "block" : "hidden"
+        } md:grid md:grid-cols-12 flex-col justify-between items-center gap-16 px-14  py-2`}>
+        <div className="flex md:col-span-8 md:flex-row flex-col w-full gap-4 justify-between items-center">
+          {["All", "Podcasts", "Stories", "Audio Book"].map((item) => (
+            <p
+              key={item}
+              className="text-center border border-white py-1 w-full rounded-md text-sm bg-black">
+              {item}
+            </p>
+          ))}
         </div>
-        <div className="bg-black py-2 w-6/12 px-4 rounded-3xl flex justify-between items-center ">
-          <p>Search</p>
-          <CiSearch className="text-xl" />
-        </div>
-        <div className="text-2xl">
-          <IoIosNotifications />
+
+        <div className="md:col-span-4 flex items-center gap-10 justify-between w-full">
+          <div className="bg-black py-2  w-full md:mt-0 mt-3 px-4 rounded-3xl flex justify-between items-center">
+            <p>Search</p>
+            <CiSearch className="text-xl" />
+          </div>
+
+          <div className="text-2xl  md:mt-0 mt-3 flex justify-end">
+            <IoIosNotifications />
+          </div>
         </div>
       </div>
     </nav>
--- podcast/src/pages/Home/Home.jsx
+++ podcast/src/pages/Home/Home.jsx
@@ -2,18 +2,30 @@ import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 
 const Home = () => {
-  const [isUserViewOpen, setIsUserViewOpen] = useState(true);
+  const [isUserViewOpen, setIsUserViewOpen] = useState(false);
 
   return (
     <>
       <Navbar />
       <div className="grid grid-cols-12 h-[100vh]">
-        <div className="col-span-2 text-gray-50 bg-[#222222]">
-          <div className={`w-full ${isUserViewOpen ? "block" : "hidden"}`}>
-            <p>non</p>
+        <div
+          className={`col-span-2 text-gray-50 bg-[#222222] ${
+            isUserViewOpen ? "fixed inset-0 z-40 w-6/12" : "hidden col-span-0"
+          } md:block md:relative md:z-0`}>
+          <div className="p-4">
+            <p>Sidebar Content</p>
           </div>
         </div>
-        <div className="col-span-10 bg-black"></div>
+
+        <div className={`md:col-span-10 col-span-12 bg-black relative`}>
+          {!isUserViewOpen && (
+            <button
+              onClick={() => setIsUserViewOpen(true)}
+              className="md:hidden p-2 bg-gray-700 rounded m-4">
+              Open Sidebar
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
