7a7478ace5d83baa2afebdf0ea578922cd2d73fd
7vTr0x7 <7vTrox7@gmail.com>
2024-11-12 11:59:23 +0530

    feat: Loading

--- podcast/src/App.jsx
+++ podcast/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { lazy, Suspense } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loading from "./components/Loading/Loading";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 
 const App = () => {
   return (
-    <Suspense fallback={<p>Loading..</p>}>
+    <Suspense fallback={<Loading />}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
