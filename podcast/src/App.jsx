8e53020e76fec23cf14fd8b7bafc1940390dbae7
7vTr0x7 <7vTrox7@gmail.com>
2024-11-12 11:37:16 +0530

    feat: routes

--- podcast/src/App.jsx
+++ podcast/src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import Home from "./pages/Home/Home";
+import React, { lazy, Suspense } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home/Home"));
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Suspense fallback={<p>Loading..</p>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 };
 
--- podcast/src/components/UserSlider/UserFeaturesNav.jsx
+++ podcast/src/components/UserSlider/UserFeaturesNav.jsx
@@ -6,37 +6,52 @@ import { FaMicrophone } from "react-icons/fa6";
 import { FaFileAudio } from "react-icons/fa";
 import { GiBookCover } from "react-icons/gi";
 import { FaHistory } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const UserFeaturesNav = () => {
   return (
     <div className="md:my-8 my-5 flex flex-col justify-between px-3">
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <MdHome className="text-xl" />
-        <p>Home</p>
+        <p>
+          <Link to="/">Home</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <IoMdDownload className="text-xl" />
-        <p>Download</p>
+        <p>
+          <Link to="/">Download</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <BsCollectionPlayFill className="text-base" />
-        <p>Library</p>
+        <p>
+          <Link to="/">Library</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <FaMicrophone className="text-lg" />
-        <p>Podcast</p>
+        <p>
+          <Link to="/">Podcast</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <FaFileAudio className="text-lg" />
-        <p>Audio Book</p>
+        <p>
+          <Link to="/">Audio Book</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <GiBookCover className="text-lg" />
-        <p>Stories</p>
+        <p>
+          <Link to="/">Stories</Link>
+        </p>
       </div>
       <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
         <FaHistory className="text-lg" />
-        <p>Listening History</p>
+        <p>
+          <Link to="/">Listening History</Link>
+        </p>
       </div>
     </div>
   );
