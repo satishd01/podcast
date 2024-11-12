224398a7af83047cd92ace825d6d36190bba8f99
7vTr0x7 <7vTrox7@gmail.com>
2024-11-12 12:24:00 +0530

    fix: tab color

--- podcast/src/components/UserSlider/UserFeaturesNav.jsx
+++ podcast/src/components/UserSlider/UserFeaturesNav.jsx
@@ -6,51 +6,82 @@ import { FaMicrophone } from "react-icons/fa6";
 import { FaFileAudio } from "react-icons/fa";
 import { GiBookCover } from "react-icons/gi";
 import { FaHistory } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const UserFeaturesNav = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  // Function to apply active classes based on the current path
+  const getNavItemClass = (path) => {
+    return currentPath === path
+      ? "bg-gray-200 text-black rounded-md mb-1"
+      : "hover:bg-gray-200 hover:text-black mb-1";
+  };
+
   return (
     <div className="md:my-8 my-5 flex flex-col justify-between px-3">
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/"
+        )}`}>
         <MdHome className="text-xl" />
         <p>
           <Link to="/">Home</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/download"
+        )}`}>
         <IoMdDownload className="text-xl" />
         <p>
-          <Link to="/">Download</Link>
+          <Link to="/download">Download</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/library"
+        )}`}>
         <BsCollectionPlayFill className="text-base" />
         <p>
-          <Link to="/">Library</Link>
+          <Link to="/library">Library</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/podcast"
+        )}`}>
         <FaMicrophone className="text-lg" />
         <p>
-          <Link to="/">Podcast</Link>
+          <Link to="/podcast">Podcast</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/audio-book"
+        )}`}>
         <FaFileAudio className="text-lg" />
         <p>
-          <Link to="/">Audio Book</Link>
+          <Link to="/audio-book">Audio Book</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/stories"
+        )}`}>
         <GiBookCover className="text-lg" />
         <p>
-          <Link to="/">Stories</Link>
+          <Link to="/stories">Stories</Link>
         </p>
       </div>
-      <div className="py-3  flex items-center gap-3 hover:bg-gray-200 px-3 hover:rounded-lg hover:text-black cursor-pointer">
+      <div
+        className={`py-3 flex items-center gap-3 px-3 hover:rounded-lg cursor-pointer ${getNavItemClass(
+          "/history"
+        )}`}>
         <FaHistory className="text-lg" />
         <p>
-          <Link to="/">Listening History</Link>
+          <Link to="/history">Listening History</Link>
         </p>
       </div>
     </div>
