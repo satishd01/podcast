59d30ee318bb6400f5b74619b79eb81921ae58a6
7vTr0x7 <7vTrox7@gmail.com>
2024-11-13 16:11:37 +0530

    feat: Genres

--- podcast/src/App.jsx
+++ podcast/src/App.jsx
@@ -2,10 +2,10 @@ import React, { lazy, Suspense } from "react";
 
 import { Route, Routes } from "react-router-dom";
 import Loading from "./components/Loading/Loading";
-import GenresPage from "./pages/GenresPage/GenresPage";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const Search = lazy(() => import("./pages/Search/Search"));
+const GenresPage = lazy(() => import("./pages/GenresPage/GenresPage"));
 
 const App = () => {
   return (
