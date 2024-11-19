e38f80e54d76bb15e4dbe730432f65db6b92cd77
7vTr0x7 <7vTrox7@gmail.com>
2024-11-19 18:42:24 +0530

    feat: MyPlaylist

--- podcast/src/components/MyPlaylist/PlaylistBody/PlaylistBody.jsx
+++ podcast/src/components/MyPlaylist/PlaylistBody/PlaylistBody.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsSortDown, BsSortUpAlt } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa";
 import Playlists from "./../Playlists/Playlists";
 import PlaylistPodcasts from "../PlaylistPodcasts/PlaylistPodcasts";
+import { playlistData } from "../../../utils/constants";
 
 const PlaylistBody = () => {
   const [toggleSort, setToggleSort] = useState(true);
+  const [isCreatePlaylistOpen, setIsCreatePlaylistOpen] = useState(false);
+  const [name, setName] = useState("");
+
+  const openCreatePlaylist = () => {
+    setIsCreatePlaylistOpen(true);
+  };
 
   return (
     <>
@@ -30,7 +37,9 @@ const PlaylistBody = () => {
           </li>
           <li className="rounded-lg border border-white px-2 py-1">Stories</li>
         </ul>
-        <div className="flex gap-3 text-[10px] md:text-sm items-center bg-white text-black  px-3 py-1 rounded-md">
+        <div
+          className="flex cursor-pointer gap-3 text-[10px] md:text-sm items-center bg-white text-black  px-3 py-1 rounded-md"
+          onClick={openCreatePlaylist}>
           <FaPlus className="text-sm" />
           <p>Create playlist</p>
         </div>
@@ -45,6 +54,27 @@ const PlaylistBody = () => {
           <PlaylistPodcasts />
         </div>
       </div>
+
+      {isCreatePlaylistOpen && (
+        <div className=" py-5 px-4 w-[30%] absolute left-[30%] top-[20%] bg-[#222222] rounded-lg">
+          <p className="text-center">Enter Playlist Name</p>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter here.."
+            className="bg-[#151515] outline-none mt-3 px-3 py-2 rounded-md w-full "
+          />
+          <div className="flex justify-end gap-3 items-center mt-3">
+            <p
+              onClick={() => setIsCreatePlaylistOpen(false)}
+              className="cursor-pointer">
+              Close
+            </p>
+            <p className="cursor-pointer">Done</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- podcast/src/components/MyPlaylist/Playlists/Playlists.jsx
+++ podcast/src/components/MyPlaylist/Playlists/Playlists.jsx
@@ -16,15 +16,17 @@ const Playlists = () => {
             key={playlist.id}
             className="flex items-center justify-between bg-[#2F2F2F] mb-4 rounded-md"
             onClick={() =>
-              navigate(`/your-playlist/${playlist.name.toLowerCase()}`)
+              navigate(`/your-playlist/${playlist?.name.toLowerCase()}`)
             }>
             <div className="flex items-center gap-4">
               <div className="bg-black p-4 rounded-md border border-white">
                 <GiBookCover className="text-2xl text-red-600" />
               </div>
               <div className="text-sm">
-                <p>{playlist.name}</p>
-                <p className="text-xs">{`${playlist.episodes} episodes`}</p>
+                <p>{playlist?.name}</p>
+                {playlist?.episodes > 0 && (
+                  <p className="text-xs">{`${playlist?.episodes} episodes`}</p>
+                )}
               </div>
             </div>
             <MdOutlineKeyboardArrowRight className="mr-4 text-xl" />
