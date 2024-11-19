5a49f23255a570e1b8efd8acd20550ce6df0776f
7vTr0x7 <7vTrox7@gmail.com>
2024-11-19 13:27:40 +0530

    feat: Library

--- podcast/src/components/LikedPodcasts/LikedList/LikedList.jsx
+++ podcast/src/components/LikedPodcasts/LikedList/LikedList.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { BsSortUpAlt } from "react-icons/bs";
+import React, { useState } from "react";
 import { IoPlay } from "react-icons/io5";
+import LikedPodcastTable from "./LikedPodcastTable";
+
+import { BsSortDown } from "react-icons/bs";
+import { BsSortUpAlt } from "react-icons/bs";
+
+import podcasts from "../../../utils/json/podcasts.json";
 
 const LikedList = () => {
+  const [toggleSort, setToggleSort] = useState(true);
+
   return (
     <div className="bg-[#101010] py-4 md:mx-2 mt-5 md:px-8 px-4">
       <div className="flex items-center justify-between">
@@ -13,16 +20,24 @@ const LikedList = () => {
           </li>
           <li className="rounded-lg border border-white px-2 py-1">Stories</li>
         </ul>
-        <div className="flex items-center md:text-base gap-3 text-sm">
-          <BsSortUpAlt className="text-lg" />
+        <div
+          className="flex items-center md:text-base gap-3 text-sm"
+          onClick={() => setToggleSort((prev) => !prev)}>
+          {toggleSort ? (
+            <BsSortUpAlt className="text-lg" />
+          ) : (
+            <BsSortDown className="text-lg" />
+          )}
           <p>Sort by</p>
         </div>
       </div>
-      <div className="inline-block mt-3">
+      <div className="inline-block mt-5">
         <div className="md:p-4 p-2 rounded-full flex items-center justify-center bg-white">
           <IoPlay className="text-[#FF0000]" />
         </div>
       </div>
+
+      <LikedPodcastTable podcasts={podcasts} toggleSort={toggleSort} />
     </div>
   );
 };
