1e337a7fda026ec36117ce5f8884f8dee866e774
7vTr0x7 <7vTrox7@gmail.com>
2024-11-28 16:20:04 +0530

    feat: ListeningHistory

--- podcast/src/components/ListeningHistory/History.jsx
+++ podcast/src/components/ListeningHistory/History.jsx
@@ -12,26 +12,29 @@ const History = () => {
         <div key={day}>
           <h2 className="text-xl my-4">{day}</h2>
           {podcasts.map((podcast, index) => (
-            <div key={index} className="flex justify-between items-center mb-3">
-              <div className="flex gap-4 md:gap-10 items-center mb-3">
-                <img
-                  src={"https://placehold.co/50"}
-                  alt={podcast.title}
-                  className="rounded-md w-12 h-12 md:w-16 md:h-16"
-                />
-                <div>
-                  <h3>{podcast.title}</h3>
-                  <p className="text-sm font-light mt-2 flex items-center gap-3">
-                    <FaUser /> {podcast.host}
-                  </p>
+            <div key={index}>
+              <div className="flex justify-between items-center mb-2 mt-1">
+                <div className="flex gap-4 md:gap-10 items-center mb-3">
+                  <img
+                    src={"https://placehold.co/50"}
+                    alt={podcast.title}
+                    className="rounded-md w-12 h-12 md:w-16 md:h-16"
+                  />
+                  <div>
+                    <h3>{podcast.title}</h3>
+                    <p className="text-sm font-light mt-2 flex items-center gap-3">
+                      <FaUser /> {podcast.host}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-4">
+                  <div className="md:p-3 p-2 rounded-full flex items-center justify-center bg-white">
+                    <IoPlay className="text-[#FF0000]" />
+                  </div>
+                  <BsThreeDotsVertical className="text-white text-xl cursor-pointer" />
                 </div>
               </div>
-              <div className="flex items-center gap-4">
-                <div className="md:p-3 p-2 rounded-full flex items-center justify-center bg-white">
-                  <IoPlay className="text-[#FF0000]" />
-                </div>
-                <BsThreeDotsVertical className="text-white text-xl cursor-pointer" />
-              </div>
+              <hr className="h-1 text-[#767676]" />
             </div>
           ))}
         </div>
