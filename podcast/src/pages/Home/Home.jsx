aac33a28d549581f6f5b16fbf21b5ca6392ddf87
7vTr0x7 <7vTrox7@gmail.com>
2024-11-29 11:25:10 +0530

    feat: Home

--- podcast/src/pages/Home/Home.jsx
+++ podcast/src/pages/Home/Home.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GenresPage from "../GenresPage/GenresPage";
+import Navbar from "../../components/Navbar/Navbar";
+import SliderDiv from "../../components/SliderDiv/SliderDiv";
+import TopCreators from "../../components/Podcasts/TopCreators/TopCreators";
+import PodcastList from "../../components/Podcasts/PodcastList/PodcastList";
+import LatestShows from "../../components/Podcasts/LatestShows/LatestShows";
+import Footer from "../../components/Footer/Footer";
+import {
+  resizeHandler,
+  scrollToTop,
+  userSliderHandler,
+} from "../../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSlider } from "../../app/slices/sliderSlice";
 
 const Home = () => {
-  
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
+  const isUserViewOpen = useSelector((state) => state.slider.isSliderOpen);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    resizeHandler(dispatch, toggleSlider);
+  }, [dispatch]);
+
+  useEffect(() => {
+    userSliderHandler(dispatch, toggleSlider, isUserViewOpen);
+  }, [isUserViewOpen, dispatch]);
+
   return (
     <div>
-      <GenresPage />
+      <Navbar />
+      <div className="grid grid-cols-12">
+        <SliderDiv />
+
+        <div
+          className={`${
+            isUserViewOpen ? "md:col-span-10" : "md:col-span-12"
+          } col-span-12 text-white bg-black relative h-auto px-4 md:px-10 py-10`}>
+          <TopCreators text={"Top Podcast Creators"} />
+          <TopCreators text={"Top Stories Creators"} />
+          <TopCreators text={"Top Audio Book Creators"} />
+          <LatestShows text={"Latest Podcasts"} />
+          <LatestShows text={"Latest Stories"} />
+          <LatestShows text={"Latest Audio Books"} />
+          <PodcastList text={"Podcasts"} />
+          <PodcastList text={"Stories"} />
+          <PodcastList text={"Audio Books"} />
+
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
